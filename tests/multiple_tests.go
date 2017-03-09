5921784b4eac249280f540c955a0b559082f8f0d
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-03-09 13:21:48 +0100

    code refactoring for multiple test
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- tests/multiple_tests.go
+++ tests/multiple_tests.go
@@ -12,22 +12,46 @@ import (
 var mu = &sync.Mutex{}
 var counter int
 
-const numberOfConnections int = 8
+const hostAddr string = "0.0.0.0"
+const user string = "root"
+const passwd string = "root"
+
+func cleanDatastore() {
+	counter = 0
+
+	xml := `
+<edit-config>
+	<target><running/></target>
+	<config xmlns:op="urn:ietf:params:xml:ns:netconf:base:1.0">
+		<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces" op:operation='delete'/>
+	</config>
+</edit-config>`
+
+	s, err := netconf.DialSSH(hostAddr, netconf.SSHConfigPassword(user, passwd))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = s.Exec(netconf.RawMethod(xml))
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+	}
+}
 
 func setTests(s *netconf.Session, wg *sync.WaitGroup, limit int) {
 
 	setXML := `
-	<edit-config>
-		<target><running/></target>
-		<config>
-			<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
-				<interface>
-					<name>ethernet_%d</name>
-					<type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:ethernetCsmacd</type>
-				</interface>
-			</interfaces>
-		</config>
-	</edit-config>`
+<edit-config>
+	<target><running/></target>
+	<config xmlns:op="urn:ietf:params:xml:ns:netconf:base:1.0">
+		<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+			<interface op:operation="create">
+				<name>ethernet_%d</name>
+				<type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:ethernetCsmacd</type>
+			</interface>
+		</interfaces>
+	</config>
+</edit-config>`
 
 	for {
 		if counter >= limit {
@@ -44,71 +68,56 @@ func setTests(s *netconf.Session, wg *sync.WaitGroup, limit int) {
 		_, err := s.Exec(netconf.RawMethod(xml))
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err)
-			panic(err)
 		}
 	}
 
 	wg.Done()
 }
 
-func main() {
-	hostAddr := "0.0.0.0"
-	user, passwd := "root", "root"
-
+func getSessions(numberOfSessions int) []*netconf.Session {
 	// number of connections
-	var s [numberOfConnections]*netconf.Session
+	var s []*netconf.Session
 
-	var err error
-	for n := 0; n < numberOfConnections; n++ {
-		s[n], err = netconf.DialSSH(hostAddr, netconf.SSHConfigPassword(user, passwd))
+	for n := 0; n < numberOfSessions; n++ {
+		session, err := netconf.DialSSH(hostAddr, netconf.SSHConfigPassword(user, passwd))
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer s[n].Close()
-
-		// need to make a netconf operation or netopeer-server will freeze after
-		// 6 or more connections
-		xml := "<commit/>"
-		_, err := s[n].Exec(netconf.RawMethod(xml))
-		if err != nil {
-			panic(err)
-		}
+		s = append(s, session)
 	}
+
+	return s
+}
+
+func closeSessions(sessions []*netconf.Session) {
+	for _, s := range sessions {
+		s.Close()
+	}
+}
+
+func main() {
+	sessions := []int{1, 2, 4}
+	limits := []int{10, 20, 40}
 
 	var wg sync.WaitGroup
-
-	fmt.Printf("\n\n\t\t%s", "set check")
-	fmt.Printf("\n%-32s| %-15s | %-10s\n",
-		"Operation", "number of items", "average time")
-
-	start := time.Now()
 	counter = 0
-	//run tests with n connections
-	for n := 0; n < numberOfConnections; n++ {
-		wg.Add(1)
-		go setTests(s[n], &wg, 10)
-	}
-	wg.Wait()
-	elapsed := time.Since(start)
-	fmt.Printf("%-32s| %-15d | %-10s\n", "set", 10, elapsed)
 
-	start = time.Now()
-	//run tests with n connections
-	for n := 0; n < numberOfConnections; n++ {
-		wg.Add(1)
-		go setTests(s[n], &wg, 50)
+	for _, numberOfSessions := range sessions {
+		sess := getSessions(numberOfSessions)
+		fmt.Printf("\n\n\t\tset check, with  %d connections", numberOfSessions)
+		fmt.Printf("\n%-32s| %-15s | %-10s\n", "Operation", "number of items", "total time")
+		fmt.Printf("-------------------------------------------------------------------\n")
+		for _, limit := range limits {
+			cleanDatastore()
+			start := time.Now()
+			for _, s := range sess {
+				wg.Add(1)
+				go setTests(s, &wg, limit)
+			}
+			wg.Wait()
+			elapsed := time.Since(start)
+			fmt.Printf("%-32s| %-15d | %-10s\n", "set", limit, elapsed)
+		}
+		closeSessions(sess)
 	}
-	wg.Wait()
-	elapsed = time.Since(start)
-	fmt.Printf("%-32s| %-15d | %-10s\n", "set", 50, elapsed)
-
-	start = time.Now()
-	//run tests with n connections
-	for n := 0; n < numberOfConnections; n++ {
-		wg.Add(1)
-		go setTests(s[n], &wg, 100)
-	}
-	wg.Wait()
-	elapsed = time.Since(start)
-	fmt.Printf("%-32s| %-15d | %-10s\n", "set", 100, elapsed)
 }
