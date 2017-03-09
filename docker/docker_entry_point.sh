1949476e7e6403f978ccd0f1bb33c72fde9a8a4e
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-03-09 16:36:27 +0100

    change entry point script
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- docker/docker_entry_point.sh
+++ docker/docker_entry_point.sh
@@ -3,4 +3,12 @@
 #add root password
 echo "root:root" | chpasswd
 
+# remove printf from the sysrepo callback in the application_example
+cd /opt/dev/sysrepo
+sed -i '63d' ./examples/application_example.c
+sed -i '61d' ./examples/application_example.c
+cd build
+make
+
+# run the sysrepo application for the yang model ietf-interfaces
 /opt/dev/sysrepo/build/examples/application_example "ietf-interfaces" &
